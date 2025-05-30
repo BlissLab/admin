c12629b5ad462c34e8b891a680404645d3857a49
jinlong <jlonmyway@gmail.com>
2025-05-31 00:58:11 +0800

    format

--- server/model/voice/request/file.go
+++ server/model/voice/request/file.go
@@ -1,18 +1,18 @@
-
 package request
 
 import (
-	"github.com/flipped-aurora/gin-vue-admin/server/model/common/request"
 	"time"
+
+	"github.com/flipped-aurora/gin-vue-admin/server/model/common/request"
 )
 
-type FileSearch struct{
-    CreatedAtRange []time.Time `json:"createdAtRange" form:"createdAtRange[]"`
-      StartDuration  *int  `json:"startDuration" form:"startDuration"`
-EndDuration  *int  `json:"endDuration" form:"endDuration"`
-      UserId  *int `json:"userId" form:"userId"` 
-      Status  *string `json:"status" form:"status"` 
-    request.PageInfo
-    Sort  string `json:"sort" form:"sort"`
-    Order string `json:"order" form:"order"`
+type FileSearch struct {
+	CreatedAtRange []time.Time `json:"createdAtRange" form:"createdAtRange[]"`
+	StartDuration  *int        `json:"startDuration" form:"startDuration"`
+	EndDuration    *int        `json:"endDuration" form:"endDuration"`
+	UserId         *int        `json:"userId" form:"userId"`
+	Status         *string     `json:"status" form:"status"`
+	request.PageInfo
+	Sort  string `json:"sort" form:"sort"`
+	Order string `json:"order" form:"order"`
 }
